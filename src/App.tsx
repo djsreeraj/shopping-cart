9de7a98d4c0ce441a0800521c8b23c7739b58669
djsreeraj <sreeraj011m@gmail.com>
2022-06-24 19:30:53 +0530

    added navigation menus

--- src/App.tsx
+++ src/App.tsx
@@ -3,16 +3,20 @@ import { Container } from "react-bootstrap";
 import { Route, Routes } from "react-router-dom";
 import Store from "./pages/Store";
 import About from "./pages/About";
+import NavBar from "./components/NavBar";
 
 function App() {
   return (
-    <Container className="mb-4">
-      <Routes>
-        <Route path="/" element={<Home />} />
-        <Route path="/store" element={<Store />} />
-        <Route path="/about" element={<About />} />
-      </Routes>
-    </Container>
+    <>
+      <NavBar />
+      <Container className="mb-4">
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/store" element={<Store />} />
+          <Route path="/about" element={<About />} />
+        </Routes>
+      </Container>
+    </>
   );
 }
 
