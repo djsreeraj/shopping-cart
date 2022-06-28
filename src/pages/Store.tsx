c5376983c227cad61bd99662733dd9ac9a125c53
djsreeraj <sreeraj011m@gmail.com>
2022-06-29 00:37:52 +0530

    added store item cards

--- src/pages/Store.tsx
+++ src/pages/Store.tsx
@@ -1,7 +1,20 @@
-import React from "react";
+import { Col, Row } from "react-bootstrap";
+import StoreItem from "../components/StoreItem";
+import storeItems from "../data/items.json"
 
 const Store = () => {
-  return <div>Store</div>;
+  return <>
+  <h1>Store</h1>
+  <Row>
+    {
+      storeItems.map( item => (
+        <Col>
+           <StoreItem  {...item} />
+        </Col>
+      ))
+    }
+  </Row>
+  </>;
 };
 
 export default Store;
